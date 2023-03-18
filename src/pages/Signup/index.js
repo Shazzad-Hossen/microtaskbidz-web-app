df1557839057a04ce9a5c7a2026960969805cfad
Shazzad Hossen <tipsfire.com@gmail.com>
2023-03-18 23:42:13 +0600

    signin page bg changed

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -9,63 +9,67 @@ const Signup = () => {
       <Header text="Sign Up" isShow="true">
         {" "}
       </Header>
-
-      <div className=" mx-auto my-6 max-w-3xl  rounded-xl bg-formbg">
-        <div className="bg-[#fff]   bg-opacity-80 p-10">
-          <form>
-            <label className="text-xl font-semibold" htmlFor="name">
-              Name
-            </label>{" "}
-            <br />
-            <input
-              className="border-5 my-2 h-12 w-[100%] rounded-lg"
-              type="text"
-              name="name"
-              id="name"
-            />
-            <label className="text-xl font-semibold" htmlFor="email">
-              Email
-            </label>{" "}
-            <br />
-            <input
-              className="border-5 my-2 h-12 w-[100%] rounded-lg"
-              type="email"
-              name="email"
-              id="email"
-            />
-            <label className="text-xl font-semibold" htmlFor="password">
-              Password
-            </label>{" "}
-            <br />
-            <input
-              className="border-5 my-2 h-12 w-[100%] rounded-lg"
-              type="password"
-              name="pass"
-              id="pass"
-            />
-            <label className="text-xl font-semibold" htmlFor="ConfirmPassword">
-              Confirm Password
-            </label>{" "}
-            <br />
-            <input
-              className="border-5 my-2 h-12 w-[100%] rounded-lg"
-              type="password"
-              name="confPass"
-              id="confPass"
-            />
-            <br /> <br />
-            <ButtonPrimary text="Sign up"></ButtonPrimary>
-            <br />
-            <br />
-            <p>
-              Already have an account ?{" "}
-              <a className="text-[#028090]" href="/user/signin">
-                Sign In here
-              </a>
-            </p>
-          </form>
+      <main>
+        <div className=" mx-auto my-6 max-w-3xl  rounded-xl bg-formbg">
+          <div className="bg-[#fff]   bg-opacity-80 p-10">
+            <form>
+              <label className="text-xl font-semibold" htmlFor="name">
+                Name
+              </label>{" "}
+              <br />
+              <input
+                className="border-5 my-2 h-12 w-[100%] rounded-lg"
+                type="text"
+                name="name"
+                id="name"
+              />
+              <label className="text-xl font-semibold" htmlFor="email">
+                Email
+              </label>{" "}
+              <br />
+              <input
+                className="border-5 my-2 h-12 w-[100%] rounded-lg"
+                type="email"
+                name="email"
+                id="email"
+              />
+              <label className="text-xl font-semibold" htmlFor="password">
+                Password
+              </label>{" "}
+              <br />
+              <input
+                className="border-5 my-2 h-12 w-[100%] rounded-lg"
+                type="password"
+                name="pass"
+                id="pass"
+              />
+              <label
+                className="text-xl font-semibold"
+                htmlFor="ConfirmPassword"
+              >
+                Confirm Password
+              </label>{" "}
+              <br />
+              <input
+                className="border-5 my-2 h-12 w-[100%] rounded-lg"
+                type="password"
+                name="confPass"
+                id="confPass"
+              />
+              <br /> <br />
+              <ButtonPrimary text="Sign up"></ButtonPrimary>
+              <br />
+              <br />
+              <p>
+                Already have an account ?{" "}
+                <a className="text-[#028090]" href="/user/signin">
+                  Sign In here
+                </a>
+              </p>
+            </form>
+          </div>
         </div>
-      </div>
+      </main>
 
       <Footer></Footer>
     </div>
