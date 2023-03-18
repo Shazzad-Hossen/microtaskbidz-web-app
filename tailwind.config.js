1619a63d505269402b0e9597e8041bde7a47493b
Shazzad Hossen <tipsfire.com@gmail.com>
2023-03-18 21:55:28 +0600

    some content added in landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        article1: "url('/src/images/article1.png')",
+        article2: "url('/src/images/article2.png')",
+        article3: "url('/src/images/article3.png')",
+      },
+    },
   },
   plugins: [],
 };
